cdeea5a385bc5a188d13230154a9ea0118762078
tboy <tosinmoses384@gmail.com>
2024-12-21 08:15:15 +0100

    remove useSearchParam

--- src/app/(nondashboard)/search/page.tsx
+++ src/app/(nondashboard)/search/page.tsx
@@ -2,29 +2,33 @@
 
 import Loading from "@/components/Loading";
 import { useGetCoursesQuery } from "@/state/api";
-import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import { useRouter, 
+  // useSearchParams 
+} from "next/navigation";
+import React, { 
+  // useEffect, 
+  useState } from "react";
 import { motion } from "framer-motion";
 import CourseCardSearch from "@/components/CourseCardSearch";
 import SelectedCourse from "./SelectedCourse";
 
 const Search = () => {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
+  // const searchParams = useSearchParams();
+  // const id = searchParams.get("id");
   const { data: courses, isLoading, isError } = useGetCoursesQuery({});
   const [selectedCourse, setSelectedCourse] = useState<Course | null>(null);
   const router = useRouter();
 
-  useEffect(() => {
-    if (courses) {
-      if (id) {
-        const course = courses.find((c) => c.courseId === id);
-        setSelectedCourse(course || courses[0]);
-      } else {
-        setSelectedCourse(courses[0]);
-      }
-    }
-  }, [courses, id]);
+  // useEffect(() => {
+  //   if (courses) {
+  //     if (id) {
+  //       const course = courses.find((c) => c.courseId === id);
+  //       setSelectedCourse(course || courses[0]);
+  //     } else {
+  //       setSelectedCourse(courses[0]);
+  //     }
+  //   }
+  // }, [courses, id]);
 
   if (isLoading) return <Loading />;
   if (isError || !courses) return <div>Failed to fetch courses</div>;
