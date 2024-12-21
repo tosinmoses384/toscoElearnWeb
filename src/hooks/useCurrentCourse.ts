6a6e630db973a92baa1e510b8a2e9d493f37e4ad
tboy <tosinmoses384@gmail.com>
2024-12-21 07:48:25 +0100

    disable lint

--- src/hooks/useCurrentCourse.ts
+++ src/hooks/useCurrentCourse.ts
@@ -1,10 +1,14 @@
-import { useGetCourseQuery } from "@/state/api";
+// import { useGetCourseQuery } from "@/state/api";
 import { useSearchParams } from "next/navigation";
 
 export const useCurrentCourse = () => {
   const searchParams = useSearchParams();
   const courseId = searchParams.get("id") ?? "";
-  const { data: course, ...rest } = useGetCourseQuery(courseId);
+  // const { data: course, ...rest } = useGetCourseQuery(courseId);
 
-  return { course, courseId, ...rest };
+  return { 
+    // course, 
+    courseId, 
+    // ...rest 
+  };
 };
